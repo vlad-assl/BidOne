dc36588c05e036f49bbb4be8e28156af3ea3f550
Vladan Neziri <vlad@accuratesoftwaresolutions.co.nz>
2021-02-18 18:27:03 +1300

    Intial Files

---
+++ BidOne/TechincalTask/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BidOne.TechincalTask
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
